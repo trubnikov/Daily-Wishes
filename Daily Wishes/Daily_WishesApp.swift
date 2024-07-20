7331fff75294c09d57efaa8b20d4ec03e31977c2
Dmitry trubnikov <d.trubnikov@me.com>
2024-07-21 00:39:07 +0300

    Initial Commit

---
+++ Daily Wishes/Daily_WishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_WishesApp.swift
+//  Daily Wishes
+//
+//  Created by Dmitry Trubnikov on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Daily_WishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
